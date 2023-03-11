b2b19163a84dc93b0b16b9db10dda3bb355b28cd
bernadettechrestella <bernadettechrestella@gmail.com>
2023-03-11 15:54:58 +0900

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import ThemeToggle from './ThemeToggle'
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import { UserAuth } from '../context/AuthContext'
 
 const NavBar = () => {
   const [nav, setNav] = useState(false)
+  const {user, logout} = UserAuth();
+  const navigate = useNavigate();
 
   const handleNav = () => {
     setNav(!nav)
+  }
+
+  const handleSignOut = async () => {
+    try {
+      await logout()
+      navigate('/')
+    } catch (e) {
+      console.log(e.message)
+    }
   }
 
   return (
@@ -18,10 +30,18 @@ const NavBar = () => {
       <div className='hidden md:block'>
         <ThemeToggle />
       </div>
-      <div className='hidden md:block'>
+      
+      {user?.email ? (
+        <div className='hidden md:block'>
+          <Link to='/account' className='p-4'>Account</Link>
+          <button onClick={handleSignOut}>Sign Out</button>
+        </div>
+      ) : (
+        <div className='hidden md:block'>
         <Link to='/signIn' className='p-4 hover:text-accent'>Sign In</Link>
         <Link to='/signUp' className='bg-button text-btnText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign Up</Link>
       </div>
+      )}
 
       {/* Menu Icon */}
       <div onClick={handleNav} className='block md:hidden cursor-pointer z-10'>
@@ -32,23 +52,29 @@ const NavBar = () => {
       <div className={nav ? 'md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10' : 'fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in duration-300'}>
         <ul className='w-full p-4'>
           <li className='border-b py-6'>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className='border-b py-6'>
-            <Link to='/'>Account</Link>
-          </li>
-          <li className='py-6'>
             <ThemeToggle />
           </li>
+          <li onClick={handleNav} className='border-b py-6'>
+            <Link to='/'>Home</Link>
+          </li>
+          {user?.email ? (
+          <li onClick={handleNav} className='border-b py-6'>
+            <Link to='/account'>Account</Link>
+          </li> ) : null }
         </ul>
+        {user?.email ? (
+          <div className='flex flex-col w-full p-4'>
+            <button onClick={handleSignOut} className='w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl'>Sign Out</button>
+          </div>
+        ) : (
         <div className='flex flex-col w-full p-4'>
           <Link to='/signIn'>
-            <button className='w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl'>Sign In</button>
+            <button onClick={handleNav} className='w-full my-2 p-3 bg-primary text-primary border border-secondary rounded-2xl shadow-xl'>Sign In</button>
           </Link>
           <Link to='/signUp'>
-            <button className='w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl'>Sign Up</button>
+            <button onClick={handleNav} className='w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl'>Sign Up</button>
           </Link>
-        </div>
+        </div> )}
       </div>
     </div>
   )
